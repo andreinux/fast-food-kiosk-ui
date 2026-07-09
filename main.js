06636c148cd54d529ef7010866274fea27d52996
unknown <andreiacojido@gmail.com>
2026-07-09 21:31:13 +0800

    created section for burger items and enabled show section

--- main.js
+++ main.js
@@ -3,17 +3,29 @@ let orderBtn = document.querySelector("#order-btn");
 
 let welcome = document.querySelector("#welcome");
 
-
-orderBtn.addEventListener("click", ()=> {
+// show and hide section function
+function showSection(id){
     let sections = document.querySelectorAll(".sections");
 
     sections.forEach(section => {
         section.setAttribute("hidden", "true");
     })
-    let active = document.getElementById("categories");
+    let active = document.getElementById(id);
     active.removeAttribute("hidden");
+    }
 
+orderBtn.addEventListener("click", ()=> {
+   showSection("categories");
 })
+
+
+
+
+let hamburgerBtn = document.querySelector("#hamburger-btn");
+
+hamburgerBtn.addEventListener("click", ()=> {
+    showSection("burger-items");
+});
 
 
 
