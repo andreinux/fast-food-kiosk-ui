479b2b111e9a9a635252e0afe4ebea84197dd675
unknown <andreiacojido@gmail.com>
2026-07-10 12:23:34 +0800

    added fucntionality in quantity encounetr responsive to itembtn click

--- main.js
+++ main.js
@@ -69,5 +69,91 @@ cheeseBurgerItembtn.addEventListener("click" , ()=> {
 
 
 
+let martiniDisplayQuantity = document.querySelector("#martini-quantity");
+let martiniItembtn = document.querySelector("#martini-itembtn");
+let martiniQuantity = Number(martiniDisplayQuantity.textContent);
+
+martiniItembtn.addEventListener("click", ()=> {
+    martiniQuantity += 1;
+    martiniDisplayQuantity.textContent = martiniQuantity;
+})
+
+let hdDisplayquantity = document.querySelector("#hotdog-quantity");
+let hotdogItembtn = document.querySelector("#hotdog-itembtn");
+let hotdogQuantity = Number(hdDisplayquantity.textContent);
+
+hotdogItembtn.addEventListener("click", ()=> {
+    hotdogQuantity += 1;
+    hdDisplayquantity.textContent = hotdogQuantity;
+})
 
 
+let flDisplayQuantity = document.querySelector("#footlong-quantity");
+let footlongItembtn = document.querySelector("#footlong");
+let flQuantity = Number(flDisplayQuantity.textContent);
+
+footlongItembtn.addEventListener("click" ,()=> {
+    flQuantity += 1;
+    flDisplayQuantity.textContent = flQuantity; 
+})
+
+let SfriesDisplayQuantity = document.querySelector("#Sfries-quantity");
+let SfriesItembtn = document.querySelector("#friesSmall-itembtn");
+let SfriesQuantity = Number(SfriesDisplayQuantity.textContent);
+
+SfriesItembtn.addEventListener("click", ()=> {
+    SfriesQuantity += 1;
+    SfriesDisplayQuantity.textContent = SfriesQuantity;
+})
+
+
+let LfriesDisplayQuantity = document.querySelector("#Lfries-quantity");
+let LfriesItembtn = document.querySelector("#friesLarge-itembtn");
+let LfriesQuantity = Number(LfriesDisplayQuantity.textContent);
+
+LfriesItembtn.addEventListener("click", ()=> {
+    LfriesQuantity += 1;
+    LfriesDisplayQuantity.textContent = LfriesQuantity;
+})
+
+
+
+let itDisplayQuantity = document.querySelector("#icedTea-quantity");
+let icedteaItembtn = document.querySelector("#icedtea-itembtn");
+let itQuantity = Number(itDisplayQuantity.textContent);
+
+icedteaItembtn.addEventListener("click", ()=> {
+    itQuantity += 1;
+    itDisplayQuantity.textContent = itQuantity;
+}) 
+
+
+
+let mochaDisplayQuantity = document.querySelector("#mocha-quantity");
+let mochaItembtn = document.querySelector("#mocha-itembtn");
+let mochaQuantity = Number(mochaDisplayQuantity.textContent);
+
+mochaItembtn.addEventListener("click", ()=> {
+    mochaQuantity += 1;
+    mochaDisplayQuantity.textContent = mochaQuantity;
+})
+
+
+let taroDisplayQuantity = document.querySelector("#taro-quantity");
+let taroItembtn = document.querySelector("#taro-itembtn");
+let taroQuantity = Number(taroDisplayQuantity.textContent);
+
+taroItembtn.addEventListener("click", ()=> {
+    taroQuantity += 1;
+    taroDisplayQuantity.textContent = taroQuantity;
+})
+
+
+let matchaDisplayQuantity = document.querySelector("#matcha-quantity");
+let matchaItembtn = document.querySelector("#matcha-itembtn");
+let matchaQuantity = Number(matchaDisplayQuantity.textContent);
+
+matchaItembtn.addEventListener("click", ()=> {
+    matchaQuantity += 1;
+    matchaDisplayQuantity.textContent = matchaQuantity;
+})
